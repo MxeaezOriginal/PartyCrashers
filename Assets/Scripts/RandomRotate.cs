8176c8eeabd192829c34bdcdc63ac6d101bd07ce
Cooperjpk <cooperjpklassen@gmail.com>
2016-08-20 00:13:25 -0700

    More Balloon projectile work

--- Assets/Scripts/RandomRotate.cs
+++ Assets/Scripts/RandomRotate.cs
@@ -3,26 +3,34 @@ using System.Collections;
 
 public class RandomRotate : MonoBehaviour {
 
-    public float rotationSpeed = 1;
-    public float rotationMax = 700;
+    public float rotationSpeed;
+    public float distanceSpeed;
+    public float offset;
 
-    public float offsetMax = 1;
-    public float offsetMin = 0.1f;
+    private Vector3 appliedVector;
 
-    private float offset;
-    private float gradualAngle = 100;
-	
-	void Update ()
+    private Vector3 radiusPoint;
+
+    void Start()
     {
 
-        offset = Random.Range(offsetMin,offsetMax);
+        appliedVector = Vector3.forward * distanceSpeed;
 
-        if (gradualAngle <= rotationMax)
-        {
-            gradualAngle += 1 * rotationSpeed;
-        }
+    }
 
-        this.transform.RotateAround(transform.position * offset, Vector3.forward, gradualAngle * Time.deltaTime);
+	void Update ()
+    {
+        spinForward();
 
-	}
+    }
+
+    void spinForward()
+    {
+
+        radiusPoint = Vector3.ClampMagnitude(radiusPoint, offset);
+
+        this.transform.RotateAround(radiusPoint, Vector3.forward, rotationSpeed * Time.deltaTime);
+        this.transform.Translate(appliedVector);
+
+    }
 }
