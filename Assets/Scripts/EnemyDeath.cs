5cae9bc2cfef29b17f60b8aeca8d16b63e441af3
Jeffery-Liu <liujinfeng1209@gmail.com>
2016-08-30 15:16:31 -0700

    !!!

---
+++ Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDeath : MonoBehaviour
+{
+
+    public int m_EnemyHealth = 100;
+    public GameObject coin;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Range")
+        {
+            if (m_EnemyHealth > 0)
+            {
+                m_EnemyHealth = m_EnemyHealth - 10;
+            }
+            else
+            {
+                gameObject.active = false;
+
+                Instantiate(coin, this.gameObject.transform.position, this.gameObject.transform.rotation);
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == ("Melee"))
+        {
+            if (m_EnemyHealth > 0)
+            {
+                m_EnemyHealth = m_EnemyHealth - 10;
+            }
+            else
+            {
+                gameObject.active = false;
+
+                Instantiate(coin, this.gameObject.transform.position, this.gameObject.transform.rotation);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUD : MonoBehaviour
+{
+    private static HUD sInstance;
+
+    public static HUD Instance
+    {
+        get
+        {
+            if (sInstance == null)
+            {
+                sInstance = FindObjectOfType<HUD>();
+            }
+            return sInstance;
+        }
+    }
+
+    public static int m_Score = 0;
+    private Text ScoreField;
+
+    private void Start()
+    {
+        GameObject go = GameObject.Find("Score");
+        if (go != null)
+        {
+            ScoreField = go.GetComponent<Text>();
+        }
+        else
+        {
+            Debug.LogError("Something horrible went wrong.");
+        }
+        HUD.Instance.AdjustScore(0);
+    }
+    public void AdjustScore(int value)
+    {
+        m_Score += value;
+        if (ScoreField != null)
+        {
+            ScoreField.text = "Score: " + m_Score;
+        }
+    }
+
+
+}
