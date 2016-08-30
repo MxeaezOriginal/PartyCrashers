4e486985a4f6e862ba67040a2f14bdb46850ef6b
Mxeaez <brodyball@shaw.ca>
2016-08-30 16:13:53 -0700

    Merged Jeffery and Brody Projects

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpItem : MonoBehaviour
+{
+
+    public Vector3 spin;
+
+    void Update()
+    {
+        transform.Rotate(spin * Time.deltaTime);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            gameObject.active = false;
+        }
+    }
+}
