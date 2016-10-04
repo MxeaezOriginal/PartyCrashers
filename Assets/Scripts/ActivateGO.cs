36f88c056e3d399760374003f4b5ed3ee0a1ea49
CrayolaCarolyn <carolyn.villemagne.gamache@gmail.com>
2016-10-03 20:27:29 -0700

    Create Tutorial 1 & Tutorial 2
    Created the level with enemies,
    Coins missing, beakable doors, and door spawning script.
    Tutorial 3 (last room) not done yet.

---
+++ Assets/Scripts/ActivateGO.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateGO : MonoBehaviour
+{
+
+    public GameObject gameobject;
+
+
+    void Start()
+    {
+
+    }
+    
+
+
+    public void OnCollisionEnter(Collider other)
+    {
+        if(other != null)
+        {
+            if(other.CompareTag("Player"))
+            {
+                gameobject.SetActive(true);
+            }
+        }
+        else
+        {
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
