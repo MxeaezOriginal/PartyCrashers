76298a3ba6247bed5956656f7597d7a0521ba6e9
KminSpencer <kminspencer@gmail.com>
2016-10-08 15:08:27 -0700

    Updated minigame
    Added Dodgeball Cannon, "Shoot" script, and Dodgeball_test prefab

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+    public float FireInterval = 1f;
+    public GameObject ShotPrefab;
+    public Transform ShotLocation;
+
+    public bool Attach;
+
+    private float LastShotTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        LastShotTime = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool canShoot = (LastShotTime + FireInterval) < Time.time;
+
+
+        if (canShoot && Input.GetButton("Fire1"))
+        {
+            //shoot!
+            if (ShotPrefab != null)
+            {
+                GameObject shot = null;
+                if (ShotLocation != null)
+                {
+                    shot = Instantiate(ShotPrefab, ShotLocation.position, ShotLocation.rotation) as GameObject;
+                }
+                else
+                {
+                    shot = GameObject.Instantiate<GameObject>(ShotPrefab);
+                    shot.transform.position = transform.position;
+                    shot.transform.forward = transform.forward;
+                    shot.transform.up = transform.up;
+
+                }
+                LastShotTime = Time.time;
+                /*if (Attach == true)
+                {
+                    shot.transform.SetParent(ShotLocation, true);
+                }*/
+            }
+        }
+
+    }
+}
