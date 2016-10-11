afad4458ec07746557e08608cc7a4a3246931833
KminSpencer <kminspencer@gmail.com>
2016-10-11 16:32:16 -0700

    Water Balloon Bow

---
+++ Assets/WaterBombCombo.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterBombCombo : MonoBehaviour
+{
+
+    public string target;
+
+    public float explosionRadius;
+
+    public float damage;
+
+    public GameObject explosionObject;
+    public GameObject explosionSpawn;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == target)
+        {
+
+            GameObject explosion;
+            explosion = (GameObject)Instantiate(explosionObject, explosionSpawn.gameObject.transform.position, explosionSpawn.gameObject.transform.rotation);
+
+
+
+
+        }
+
+    }
+
+    
+
+
+}
