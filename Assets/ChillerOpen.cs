d9b8301fd06577210c92c0383ab12ba23fe79f76
Cooperjpk <cooperjpklassen@gmail.com>
2016-10-13 20:36:27 -0700

    Making a chest!

---
+++ Assets/ChillerOpen.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChillerOpen : MonoBehaviour {
+
+    public GameObject[] items;
+    public GameObject chiller;
+
+    public string playerTag;
+
+    public float item;
+
+    void Start()
+    {
+
+        item = items.GetLength(0);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == playerTag)
+        {
+
+            GiveItem();
+
+            chiller.SetActive(false);
+
+        }
+
+    }
+
+    void GiveItem()
+    {
+
+        GameObject[] loot = new GameObject[Random.Range(0,)];
+
+    }
+}
