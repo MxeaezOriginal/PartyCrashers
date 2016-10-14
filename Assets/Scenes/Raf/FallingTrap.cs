2bdb20fef61a82085ad1ab5d24de9e6bffa24b65
LunaQ <rafagda@live.com>
2016-10-13 21:40:38 -0700

    Add Falling Trap

---
+++ Assets/Scenes/Raf/FallingTrap.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingTrap : MonoBehaviour {
+
+    public float FallingSpeed;
+
+    private float mass = 1000;
+
+    GameObject mTrap;
+    
+    void Start ()
+    {
+        mTrap = this.gameObject;
+	}
+	
+	void Update ()
+    {
+	 
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Falling trap Activated.");
+
+            Rigidbody rb = mTrap.GetComponent<Rigidbody>();
+            rb.mass = mass;
+            if(FallingSpeed >= 1000)
+            {
+                rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+            }
+            rb.AddForce(Physics.gravity * (FallingSpeed * 100));
+            rb.useGravity = true;
+        }
+    }
+}
