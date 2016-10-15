e2c4bca7880d9f1d5fd1ecd92d6c25c183ac00c3
lerahk <jamie_shaw@shaw.ca>
2016-10-15 12:23:32 -0700

    breakable door

---
+++ Assets/Scripts/BreakableDoor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCollisionEnter(Collision coll)
+    {
+
+        if (coll.gameObject.tag == "Projectile")
+        {
+            Destroy(gameObject);
+        }
+        else if (coll.gameObject.tag == "Melee")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
