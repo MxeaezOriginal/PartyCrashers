c353eaadeb904dfe747c08be1e56fdab64c1b0cd
lerahk <jamie_shaw@shaw.ca>
2016-10-15 16:43:12 -0700

    Breakable door complete
    Has explosion particle effect

--- Assets/Scripts/BreakableDoor.cs
+++ Assets/Scripts/BreakableDoor.cs
@@ -3,25 +3,25 @@ using System.Collections;
 
 public class BreakableDoor : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float explosionRadius;
+    public GameObject explosionObject;
+    public GameObject particleLocation;
 
     public void OnCollisionEnter(Collision coll)
     {
 
         if (coll.gameObject.tag == "Projectile")
         {
+            GameObject explosion;
+            explosion = (GameObject)Instantiate(explosionObject, particleLocation.gameObject.transform.position, particleLocation.gameObject.transform.rotation);
+            explosion.transform.localScale = new Vector3(explosionRadius,explosionRadius,explosionRadius);
             Destroy(gameObject);
         }
         else if (coll.gameObject.tag == "Melee")
         {
+            GameObject explosion;
+            explosion = (GameObject)Instantiate(explosionObject, particleLocation.gameObject.transform.position, particleLocation.gameObject.transform.rotation);
+            explosion.transform.localScale = new Vector3(explosionRadius, explosionRadius, explosionRadius);
             Destroy(gameObject);
         }
 
