ec418bb610c0b8d9fca2dcebaced205d59247330
lerahk <james1996shaw@gmail.com>
2016-10-17 20:16:03 -0700

    Mosht updates
    wooo~

--- Assets/Scripts/Minigame/Mosh_Royale_MinigameMaster.cs
+++ Assets/Scripts/Minigame/Mosh_Royale_MinigameMaster.cs
@@ -4,7 +4,14 @@ using System.Collections;
 public class Mosh_Royale_MinigameMaster : MonoBehaviour {
 
     public GameObject moshpitEnemy;
-    public GameObject enemySpawnpoint;
+    public GameObject enemySpawnpoint1;
+    public GameObject enemySpawnpoint2;
+    public GameObject enemySpawnpoint3;
+    public GameObject enemySpawnpoint4;
+    public GameObject enemySpawnpoint5;
+    public GameObject enemySpawnpoint6;
+    public GameObject enemySpawnpoint7;
+    public GameObject enemySpawnpoint8;
     private float difficultyTimer;//seconds passed
     private float incrementAmmount;
 
@@ -12,15 +19,16 @@ public class Mosh_Royale_MinigameMaster : MonoBehaviour {
 	void Start () 
     {
         difficultyTimer = 0;
-        GameObject enemyTest;
-        enemyTest = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint.gameObject.transform.position, enemySpawnpoint.gameObject.transform.rotation );
-         
-	
+        GameObject enemyTest1;
+        enemyTest1 = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint1.gameObject.transform.position, enemySpawnpoint1.gameObject.transform.rotation );
+        enemyTest1 = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint2.gameObject.transform.position, enemySpawnpoint2.gameObject.transform.rotation);
+        enemyTest1 = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint3.gameObject.transform.position, enemySpawnpoint3.gameObject.transform.rotation);
+        enemyTest1 = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint4.gameObject.transform.position, enemySpawnpoint4.gameObject.transform.rotation);
+        enemyTest1 = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint5.gameObject.transform.position, enemySpawnpoint5.gameObject.transform.rotation );
+        enemyTest1 = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint6.gameObject.transform.position, enemySpawnpoint6.gameObject.transform.rotation);
+        enemyTest1 = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint7.gameObject.transform.position, enemySpawnpoint7.gameObject.transform.rotation);
+        enemyTest1 = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint8.gameObject.transform.position, enemySpawnpoint8.gameObject.transform.rotation);
+       
 	}
 	
-	// Update is called once per frame
-	void Update () 
-    {
-	
-	}
 }
