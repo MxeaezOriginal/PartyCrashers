79ca66ac3cad439014fc90e5c377f09e0724099c
lerahk <james1996shaw@gmail.com>
2016-10-17 19:50:12 -0700

    Ballroom blitz pit version and Mosh Royale early build

---
+++ Assets/Scripts/Minigame/Mosh_Royale_MinigameMaster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mosh_Royale_MinigameMaster : MonoBehaviour {
+
+    public GameObject moshpitEnemy;
+    public GameObject enemySpawnpoint;
+    private float difficultyTimer;//seconds passed
+    private float incrementAmmount;
+
+	// Use this for initialization
+	void Start () 
+    {
+        difficultyTimer = 0;
+        GameObject enemyTest;
+        enemyTest = (GameObject)Instantiate( moshpitEnemy, enemySpawnpoint.gameObject.transform.position, enemySpawnpoint.gameObject.transform.rotation );
+         
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
