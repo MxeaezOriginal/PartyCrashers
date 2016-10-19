9d8bba7c4bb29b2c5a430652dbe0d4c60c278d72
Jeffery-Liu <liujinfeng1209@gmail.com>
2016-10-18 20:16:06 -0700

    Still edited camara script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -54,12 +54,10 @@ public class CameraController : MonoBehaviour {
                     
                         if (players[i].transform.position.y < players[j].transform.position.y)
                         {
-                            //y = players[i].transform.position.y;
                             y = iRounded;
                         }
                         if (players[i].transform.position.y > players[j].transform.position.y)
                         {
-                            //y = players[j].transform.position.y;
                             y = jRounded;
                         }
                         
@@ -75,66 +73,22 @@ public class CameraController : MonoBehaviour {
             averageX = (x1 + x2) / 2;
             averageZ = (z1 + z2) / 2;
         }
-        //else
-        //{
-        //    for (int i = 0; i < players.Length; i++)
-        //    {
-        //        for (int j = i; j < players.Length; j++)
-        //        {
-        //            if (Mathf.Abs(players[i].transform.position.x - players[j].transform.position.x) > xDistance) // if the distance is greater than current
-        //            {
-        //                xDistance = Mathf.Abs(players[i].transform.position.x - players[j].transform.position.x);
-        //                x1 = players[i].transform.position.x; // update x to the new greatest distance
-        //                x2 = players[j].transform.position.x; // set this gamobject to the other player that this player has the greatest distance with
-        //            }
-
-        //            CharacterController iController = players[i].GetComponent<CharacterController>();
-        //            if (iController.isGrounded)
-        //            {
-        //                float iRounded = Mathf.Round(players[i].transform.position.y * 10f) / 10f;
-        //                y = iRounded;
-        //            }
-        //            if (Mathf.Abs(players[i].transform.position.z - players[j].transform.position.z) > zDistance)
-        //            {
-        //                zDistance = Mathf.Abs(players[i].transform.position.z - players[j].transform.position.z);
-        //                z1 = players[i].transform.position.z;
-        //                z2 = players[j].transform.position.z;
-        //            }
-        //        }
-        //    }
-        //}
         else
         {
             for (int i = 0; i < players.Length; i++)
             {
-                    xDistance = players[i].transform.position.x; // update x to the new greatest distance
-                    //x2 = players[j].transform.position.x; // set this gamobject to the other player that this player has the greatest distance with
-                
-
+                xDistance = players[i].transform.position.x; // update x to the new greatest distance
                 CharacterController iController = players[i].GetComponent<CharacterController>();
                 if (iController.isGrounded)
                 {
                     float iRounded = Mathf.Round(players[i].transform.position.y * 10f) / 10f;
                     y = iRounded;
                 }
-                
-                    zDistance = players[i].transform.position.z;
-                    //z2 = players[j].transform.position.z;
-                
-                
+                zDistance = players[i].transform.position.z;
             }
             averageX = xDistance;
             averageZ = zDistance;
         }
-
-        //float averageX = (x1 + x2) / 2;
-        //float averageZ = (z1 + z2) / 2;
-        //float Y = y;
-        
-
         gameObject.transform.position = new Vector3(averageX, Mathf.Lerp(transform.position.y, y + height, .1f) , averageZ - distanceOffset);
-        //Y + height
-
-       
     }
 }
