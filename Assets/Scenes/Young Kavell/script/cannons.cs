49edfe4061185aab8ed048f9d37672f64d6e7769
Kyoung2725 <lightningx2725@gmail.com>
2016-10-22 15:24:32 -0700

    Break to the beat minigame first draft work

---
+++ Assets/Scenes/Young Kavell/script/cannons.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cannons : MonoBehaviour {
+	[SerializeField]  Transform FirePoint_Location;
+	[SerializeField] float minFireRate;
+	[SerializeField] float maxFireRate;
+	[SerializeField] float fireSpeed;
+	[SerializeField] GameObject ProjectilePrefab;
+	private float currentFireRate;
+
+
+
+	// Update is called once per frame
+	void Start () 
+	{
+
+		beginFire ();
+	}
+	void beginFire()
+	{
+		currentFireRate = Random.Range(minFireRate, maxFireRate+1);
+		StartCoroutine(beginFireShot(currentFireRate));
+	}
+	IEnumerator beginFireShot(float settime)
+	{
+		yield return new WaitForSeconds(settime);
+		fire ();
+		beginFire ();
+	}
+
+	void fire()
+	{
+		GameObject dodgeball;
+		dodgeball = (GameObject)Instantiate( ProjectilePrefab, FirePoint_Location.gameObject.transform.position,FirePoint_Location.gameObject.transform.rotation);
+		dodgeball.GetComponent<Rigidbody>().AddForce(dodgeball.transform.forward * fireSpeed);
+	}
+}
