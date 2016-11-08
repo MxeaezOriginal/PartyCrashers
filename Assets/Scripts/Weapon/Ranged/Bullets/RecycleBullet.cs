9ccfab167aed3c3d464eead05baebf7fd598071f
LunaQ <rafagda@live.com>
2016-11-07 22:38:28 -0800

    BulletShtuff

---
+++ Assets/Scripts/Weapon/Ranged/Bullets/RecycleBullet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecycleBullet : MonoBehaviour
+{
+    [SerializeField]
+    private float bulletLifeTimer;
+
+    public void Start()
+    {
+        StartCoroutine(DeactivateCallback());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Coins")
+            OffBullet();
+    }
+
+    private IEnumerator DeactivateCallback()
+    {
+        yield return new WaitForSeconds(bulletLifeTimer);
+        OffBullet();
+    }
+
+    private void OffBullet()
+    {
+        // Desactivate this game object, need to create bullets pool
+        // gameObject.SetActive(false); // Deactivated until pool is ready
+
+        // Temporary Meassure
+        Destroy(gameObject);
+    }
+}
