62f619222f96417e4bb0a97941fcd5bd35ae9931
Cooperjpk <cooperjpklassen@gmail.com>
2016-11-17 21:22:54 -0800

    Moving flashing color etc. lights
    Lights, they move and or flash and or rotate and shift colors WOOOOO

---
+++ Assets/Scripts/Moving Lights/LightChangeColour.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightChangeColour : MonoBehaviour
+{
+
+    public Light lt;
+    public Color ColorOne;
+    public Color ColorTwo;
+    public float colorSwapTime;
+    private Color CurrentColor;
+    private Color PreviousColor;
+    public int CurrentColorNumber;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        CurrentColor = ColorOne;
+        PreviousColor = ColorTwo;
+        CurrentColorNumber = 1;
+        lt = GetComponent<Light>();
+        //changeColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       lt.color = Color.Lerp(CurrentColor, PreviousColor, Mathf.PingPong(Time.time, 1));
+
+        //For future reference, to make it only go one direction you will need to raise the variable ie.
+        // lt.color = Color.Lerp(CurrentColor, PreviousColor,  "0.1 value raised by x until 1");
+
+    }
+
+    void changeColor()
+    {
+        if (CurrentColorNumber == 1)
+        {
+            CurrentColorNumber = 2;
+            CurrentColor = ColorTwo;
+            PreviousColor = ColorOne;
+        }
+        else if  (CurrentColorNumber == 2)
+        {
+                CurrentColorNumber = 1;
+                CurrentColor = ColorOne;
+            PreviousColor = ColorTwo;
+        }
+
+        Invoke("changeColor", colorSwapTime);
+
+    }
+}
+
---
+++ Assets/Scripts/Moving Lights/LightFlash.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlash : MonoBehaviour
+{
+    public Light lt;
+    public Color ColorOne;
+    public Color ColorTwo;
+    public float colorSwapTime;
+    private Color CurrentColor;
+    private Color PreviousColor;
+    public int CurrentColorNumber;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        CurrentColor = ColorOne;
+        CurrentColorNumber = 1;
+        lt = GetComponent<Light>();
+        changeColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lt.color  = (CurrentColor);
+
+
+    }
+
+    void changeColor()
+    {
+        if (CurrentColorNumber == 1)
+        {
+            CurrentColorNumber = 2;
+            CurrentColor = ColorTwo;
+            PreviousColor = ColorOne;
+        }
+        else if (CurrentColorNumber == 2)
+        {
+            CurrentColorNumber = 1;
+            CurrentColor = ColorOne;
+            PreviousColor = ColorTwo;
+        }
+
+        Invoke("changeColor", colorSwapTime);
+
+    }
+
+
+}
