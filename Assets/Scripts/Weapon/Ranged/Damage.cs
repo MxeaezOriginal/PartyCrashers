be4ae818612c677506953b66654ab042cf9dde9b
Mxeaez <brodyball@shaw.ca>
2016-11-19 19:56:06 -0800

    Revert "Revert "Update""
    This reverts commit dda6d9e7b4f3d446aa24d87e7946c7a94a331101.

---
+++ Assets/Scripts/Weapon/Ranged/Damage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+
+    private float damage;
+
+    // call enemy health script
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy" || other.tag == "MeleeEnemy")
+        {
+            // substract damage from enemy health script
+        }
+    }
+	
+	
+}
