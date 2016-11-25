ba09d6b918977e32549b3dfe266fc133d0eff2a4
Skadanton <Skadanton>
2016-11-24 20:28:39 -0800

    Danilo's Pause Menu Added

---
+++ Assets/Scripts/UI/PauseGame.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PauseGame : MonoBehaviour
+{
+    public Transform canvas;
+    public Transform pauseMenu;
+    public Transform optionsMenu;
+    public Transform quitMenu;
+    //public Transform controlsMenu;
+    public Transform Player;
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+    public void Pause()
+    {
+        if (canvas.gameObject.activeInHierarchy == false)
+        {
+            if (pauseMenu.gameObject.activeInHierarchy == false)
+            {
+                pauseMenu.gameObject.SetActive(true);
+                optionsMenu.gameObject.SetActive(false);
+                quitMenu.gameObject.SetActive(false);
+                //controlsMenu.gameObject.SetActive(false);
+            }
+            canvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+        else
+        {
+            canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+           
+        }
+    }
+
+    public void Resume(bool Open)
+    {
+        if (Open)
+        {
+            //optionsMenu.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+        if (!Open)
+        {
+            //optionsMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void Options(bool Open)
+    {
+        if (Open)
+        {
+            optionsMenu.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+        }
+        if (!Open)
+        {
+            optionsMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+
+
+    public void Quit(bool Open)
+    {
+        if (Open)
+        {
+            quitMenu.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+        }
+        if (!Open)
+        {
+            quitMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+
+    /*
+    public void Controls(bool Open)
+    {
+        if (Open)
+        {
+            controlsMenu.gameObject.SetActive(true);
+            pauseMenu.gameObject.SetActive(false);
+        }
+        if (!Open)
+        {
+            controlsMenu.gameObject.SetActive(false);
+            pauseMenu.gameObject.SetActive(true);
+        }
+    }
+    */
+}
