46cd1e11d1dc10ed320d6c4a2acf21093d8b05a3
NGAEVA <ywt1230@163.com>
2016-11-28 22:18:23 -0800

    CoinScript working

---
+++ Assets/Scripts/CoinPickUps.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CoinPickUps : MonoBehaviour {
+
+    public enum Type
+    {
+        Simple,
+        Death
+    }
+    public int gold;
+    public Type type;
+    private Player m_player;
+    private PartyBar partyBar;
+
+    void Start()
+    {
+        if (GameObject.Find("PartyBar_Game").transform.FindChild("Content") != null)
+        {
+            partyBar = GameObject.Find("PartyBar_Game").transform.FindChild("Content").GetComponent<PartyBar>();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player>() != null)
+        {
+            m_player = other.GetComponent<Player>();
+            if (other.tag == "Player")
+            {
+                m_player.m_Gold += gold;
+                m_player.m_Score += 5;
+
+                if (partyBar != null)
+                {
+                    partyBar.m_Current += gold;
+                }
+
+                if (type == Type.Death)
+                {
+                    //lose health
+                }
+
+                gameObject.SetActive(false);
+            }
+
+            if (SceneManager.GetActiveScene().name == GameManager.m_Instance.m_TutorialLevel)
+            {
+                GameManager.m_Instance.m_TutorialCoins.Add(m_player.gameObject.name);
+            }
+        }
+        //Collectible collectible = other.GetComponent<Collectible>();
+
+        //other.gameObject.SetActive(false);
+
+        //tutorial
+    }
+}
--- Assets/Scripts/CollectObjects.cs
+++ Assets/Scripts/CollectObjects.cs
@@ -4,7 +4,6 @@ using System.Collections;
 
 public class CollectObjects : MonoBehaviour
 {
-
     private Player player;
     private PartyBar partyBar;
 
@@ -21,7 +20,7 @@ public class CollectObjects : MonoBehaviour
     {
         Collectible collectible = other.GetComponent<Collectible>();
 
-        if (collectible != null)
+        if(collectible != null)
         {
             player.m_Gold += collectible.gold;
             player.m_Score += 5;
@@ -31,17 +30,22 @@ public class CollectObjects : MonoBehaviour
                 partyBar.m_Current += collectible.gold;
             }
 
-            if(collectible.type == Collectible.Type.Death)
+            if (collectible.type == Collectible.Type.Death)
             {
                 //lose health
             }
+
             other.gameObject.SetActive(false);
 
-            //tutorial
-            if(SceneManager.GetActiveScene().name == GameManager.m_Instance.m_TutorialLevel)
+            if (SceneManager.GetActiveScene().name == GameManager.m_Instance.m_TutorialLevel)
             {
                 GameManager.m_Instance.m_TutorialCoins.Add(other.gameObject.name);
             }
+
+            
+
+            //tutorial
         }
+
     }
 }
