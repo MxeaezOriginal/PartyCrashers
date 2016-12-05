2fff0775bf61ddff8f4787743cb7b7c7c3c438fc
lerahk <jamie_shaw@shaw.ca>
2016-12-04 19:16:12 -0800

    LightTrigger progress

---
+++ Assets/LightTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTrigger : MonoBehaviour {
+
+    public Light[] lt;
+    private bool lightOn;
+    private int I;
+    // Use this for initialization
+    void Start ()
+    {
+        lightOn = false;
+        while (lt.Length > (I + 1) )
+        {
+            lt[I].GetComponent<Light>().enabled = false;
+            I++;
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(lightOn = false && other.gameObject.tag == "Player")
+        {
+            I = 0;
+            while (lt.Length > (I + 1))
+            {
+                lt[I].GetComponent<Light>().enabled = true;
+                I++;
+            }
+        }
+       
+        lightOn = true;
+    }
+
+}
