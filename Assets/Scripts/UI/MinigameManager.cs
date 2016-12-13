87da4a9a3c40421e0f96696ec7cd83da87119b4c
Skadanton <375449@AIV.EDMC.STU>
2016-12-12 20:59:40 -0800

    MinigameManager Script refined

--- Assets/Scripts/UI/MinigameManager.cs
+++ Assets/Scripts/UI/MinigameManager.cs
@@ -4,37 +4,38 @@ using System.Collections;
 
 public class MinigameManager : MonoBehaviour
 {
-    public CanvasGroup fadingCanvas;
-    public CanvasGroup rewardFadeInCanvas;
-
-    public Sprite[] startCountdownTextures;
-    Image startCountdownImage;
-
-    float minigameTimer = 0f;
-    bool minigameStarted;
+    float timeLeft = 1f;
+    float fadeTime = 2f;
     bool minigameEnded;
+    public float minigameTimer = 0f;
+
+    CanvasGroup fadingCanvas;
+    CanvasGroup rewardFadeInCanvas;
+    Image startCountdownImage;
+    public Sprite[] startCountdownTextures;
 
     void Awake()
     {
+        fadingCanvas = GameObject.Find("Fading Canvas").GetComponent<CanvasGroup>();
         startCountdownImage = GameObject.Find("Start Countdown Image").GetComponent<Image>();
-    }
-    void Start()
-    {
-        minigameStarted = true;
     }
     void Update()
     {
-        if (minigameStarted)
+        if (!minigameEnded)
         {
             minigameTimer += Time.deltaTime;
             MinigameStart();
         }
-
-        if (minigameEnded)
+        else
         {
             MinigameEnd();
         }
 
+        //Minigame ends in 30s from start (5s is from 3, 2, 1 Countdown in MinigameStart())
+        if (minigameTimer >= 35f)
+            minigameEnded = true;
+
+        //TESTING
         if (Input.GetKeyDown(KeyCode.T))
         {
             minigameEnded = true;
@@ -44,33 +45,37 @@ public class MinigameManager : MonoBehaviour
     void MinigameStart()
     {
         //Disable Players' movement
-        GameManager.m_Instance.m_Players[0].GetComponent<PlayerController>().m_CantMove = true;
+        foreach (GameObject player in GameManager.m_Instance.m_Players)
+            player.GetComponent<PlayerController>().m_CantMove = true;
 
-        if (minigameTimer >= 1.5f && minigameTimer <= 2.2f) { startCountdownImage.enabled = true; startCountdownImage.sprite = startCountdownTextures[0]; }
-        else if (minigameTimer >= 2.2f && minigameTimer <= 2.9f) startCountdownImage.sprite = startCountdownTextures[1];
-        else if (minigameTimer >= 2.9 && minigameTimer <= 3.6) startCountdownImage.sprite = startCountdownTextures[2];
-        else if (minigameTimer >= 3.6f && minigameTimer <= 4.3f) startCountdownImage.sprite = startCountdownTextures[3];
-        else if (minigameTimer >= 4.6f) startCountdownImage.enabled = false;
+        //3, 2, 1, GO Countdown
+        if (minigameTimer >= 1.5f && minigameTimer <= 2.3f) { startCountdownImage.enabled = true; startCountdownImage.sprite = startCountdownTextures[0]; }
+        else if (minigameTimer >= 2.3f && minigameTimer <= 3.1f) startCountdownImage.sprite = startCountdownTextures[1];
+        else if (minigameTimer >= 3.1 && minigameTimer <= 3.9) startCountdownImage.sprite = startCountdownTextures[2];
+        else if (minigameTimer >= 3.9f && minigameTimer <= 4.7f) startCountdownImage.sprite = startCountdownTextures[3];
+        else if (minigameTimer >= 5.0f)
+        {
+            startCountdownImage.enabled = false;
 
-        //Enable Players'  movement
-        //Start Minigame
+            //Enable Players' movement
+            foreach (GameObject player in GameManager.m_Instance.m_Players)
+                player.GetComponent<PlayerController>().m_CantMove = false;
+        }
     }
 
     void MinigameEnd()
     {
-            //Disable Players' movement
-            //Stop Minigame
+        foreach (GameObject player in GameManager.m_Instance.m_Players)
+            player.GetComponent<PlayerController>().m_CantMove = true;
 
-            float timeLeft = 1f;
-            float fadeTime = 2f;
-
-            timeLeft -= Time.deltaTime;
-            if (timeLeft < 0)
+        //Black Background Fading Canvas
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            if (fadingCanvas.alpha < 0.6)
             {
-                if (fadingCanvas.alpha < 0.6)
-                {
-                    fadingCanvas.alpha += Time.deltaTime / fadeTime;
-                }
+                fadingCanvas.alpha += Time.deltaTime / fadeTime;
             }
         }
+    }
 }
