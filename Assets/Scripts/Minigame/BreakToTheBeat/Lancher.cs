f5b2e73dd62838bf8169aec18da452a5a62beb43
Jeffery-Liu <liujinfeng1209@gmail.com>
2017-01-10 15:06:06 -0800

    RandomVaseLauncher

--- Assets/Scripts/Minigame/BreakToTheBeat/Lancher.cs
+++ Assets/Scripts/Minigame/BreakToTheBeat/Lancher.cs
@@ -3,40 +3,72 @@ using System.Collections;
 
 public class Lancher : MonoBehaviour
 {
-    public float FireIntervalMin = 1f;
-    public float FireIntervalMax = 20f;
-    public GameObject VasePrefeb;
-    public GameObject VasePrefeb1;
-    public GameObject VasePrefeb2;
-    public Transform ShotPos;
-    private float LastShotTime;
+    public float m_FireIntervalMin = 5f;
+    public float m_FireIntervalMax = 20f;
+    public GameObject m_VasePrefeb;
+    public GameObject m_VasePrefeb1;
+    public GameObject m_VasePrefeb2;
+    public Transform m_ShotPos;
+    private float m_LastShotTime;
+    private int m_ramdom;
+    public int m_GoldPer = 20;
+    public int m_BlackPer = 20;
+    //private float Timer;
     // Use this for initialization
     void Start()
     {
-        LastShotTime = Time.time;
+        
+        
+        m_LastShotTime = Time.time;
+        //Timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool canShoot = (LastShotTime + Random.Range(FireIntervalMin, FireIntervalMax)) < Time.time;
+        //Timer += Time.deltaTime;
+        //if(Timer >= 10 && Timer < 20)
+        //{
+        //    //m_FireIntervalMin
+        //}
+        //if(Timer >= 20 && Timer < 30)
+        //{
+
+        //}
+        //if(Timer >= 30)
+        //{
+        //    // game end
+        //}
+        bool canShoot = (m_LastShotTime + Random.Range(m_FireIntervalMin, m_FireIntervalMax)) < Time.time;
         // Shoot!
         if (canShoot)
         {
-            if (VasePrefeb != null)
+            if (m_VasePrefeb != null && m_VasePrefeb1 != null && m_VasePrefeb2 != null)
             {
-                if (ShotPos != null)
+                if (m_ShotPos != null)
                 {
-                    GameObject shot = Instantiate(VasePrefeb, ShotPos.position, ShotPos.rotation) as GameObject;
+                    m_ramdom = Random.Range(0, 100);
+                    if (m_ramdom >= 0 && m_ramdom < 60)
+                    {
+                        GameObject shot = Instantiate(m_VasePrefeb, m_ShotPos.position, m_ShotPos.rotation) as GameObject;
+                    }
+                    if (m_ramdom >= 60 && m_ramdom < 80)
+                    {
+                        GameObject shot = Instantiate(m_VasePrefeb1, m_ShotPos.position, m_ShotPos.rotation) as GameObject;
+                    }
+                    if (m_ramdom >= 80 && m_ramdom <= 100)
+                    {
+                        GameObject shot = Instantiate(m_VasePrefeb2, m_ShotPos.position, m_ShotPos.rotation) as GameObject;
+                    }
                 }
                 //else
                 //{
-                //    GameObject shot = GameObject.Instantiate<GameObject>(VasePrefeb);
+                //    GameObject shot = GameObject.Instantiate<GameObject>(m_VasePrefeb);
                 //    shot.transform.position = transform.position;
                 //    shot.transform.forward = transform.forward;
                 //    shot.transform.up = transform.up;
                 //}
-                LastShotTime = Time.time;
+                m_LastShotTime = Time.time;
             }
         }
     }
