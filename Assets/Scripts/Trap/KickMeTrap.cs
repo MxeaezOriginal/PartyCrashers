f5d7250b4df5a127ea25b55c270877d8f8b022ba
Jeffery-Liu <liujinfeng1209@gmail.com>
2017-01-10 17:10:09 -0800

    KickMeTrap fixed

--- Assets/Scripts/Trap/KickMeTrap.cs
+++ Assets/Scripts/Trap/KickMeTrap.cs
@@ -8,6 +8,7 @@ public class KickMeTrap : Trap {
     public float m_Radius;
     public float chaseIncrease;
     public float m_ChasingTime = 5.0f;
+	public bool m_used = false;
 
 	// Use this for initialization
 	void Start () {
@@ -15,35 +16,44 @@ public class KickMeTrap : Trap {
 
     public void OnTriggerStay(Collider other)
     {
-        Debug.Log("RUNING");
-        if (other.tag == "Player")
-        {
-            //EnemyAI.Instance.getDistance(50);
-            Collider[] hitColliders = Physics.OverlapSphere(transform.position, m_Radius);
-            for (int i = 0; i < hitColliders.Length; ++i)
-            {
-                for (int j = 0; j < m_EnemiesToAffect.Length; j++)
-                {
-                    if (hitColliders[i].gameObject.CompareTag(m_EnemiesToAffect[j]))
-                    {
-                        if (hitColliders[i].GetComponent<ChaserEnemyAi>() != null)
-                        {
-                            //Transform enemy = hitColliders[i].transform;
-                            ChaserEnemyAi ai = hitColliders[i].GetComponent<ChaserEnemyAi>();
+		//Debug.Log("RUNING");
+		if (other.tag == "Player" && m_used == false)
+		{
+			//EnemyAI.Instance.getDistance(50);
+			Collider[] hitColliders = Physics.OverlapSphere(transform.position, m_Radius);
+			for (int i = 0; i < hitColliders.Length; ++i)
+			{
+				for (int j = 0; j < m_EnemiesToAffect.Length; j++)
+				{
+					if (hitColliders[i].gameObject.CompareTag(m_EnemiesToAffect[j]))
+					{
+						if (hitColliders[i].GetComponent<ChaserEnemyAi>() != null)
+						{
+							//Transform enemy = hitColliders[i].transform;
+							ChaserEnemyAi ai = hitColliders[i].GetComponent<ChaserEnemyAi>();
 
-                            ai.m_ChaseDist += chaseIncrease;
-                            StartCoroutine(setToDefault(ai));
-                        }
-                    }
-                }
-            }
-        }
+							ai.m_ChaseDist += chaseIncrease;
+							StartCoroutine(setToDefault(ai));
+						}
+					}
+				}
+			}
+		}
     }
+
+	public void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			m_used = true;
+		}
+	}
 
     IEnumerator setToDefault(ChaserEnemyAi ai)
     {
         yield return new WaitForSeconds(m_ChasingTime);
         ai.m_ChaseDist -= chaseIncrease;
+
     }
 
     void OnDrawGizmosSelected()
