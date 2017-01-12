2c6e4714455c30d1d114378d54cde8942640a096
KminSpencer <kminspencer@gmail.com>
2017-01-11 19:03:33 -0800

    Boss setup

---
+++ Assets/Scripts/AI/AdvancedBossAi.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdvancedBossAi : MonoBehaviour {
+
+    enum states
+    {
+        idle,
+        rocket,
+        hurt,
+        dead,
+        count
+    }
+
+    private bool m_Invincible;
+    states state;
+
+	// Use this for initialization
+	void Start () {
+        state = states.idle;
+        m_Invincible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //Switch states
+        switch (state){
+            case states.idle: Idle();break;
+        }
+	}
+
+    void Idle()
+    {
+
+    }
+
+    void Hurt()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+    }
+}
