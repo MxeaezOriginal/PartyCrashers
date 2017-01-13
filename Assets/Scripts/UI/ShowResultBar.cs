30999eae1c1764b5f0f6f15a018d72cba3dcc1ad
Skadanton <Skadanton@gmail.com>
2017-01-12 16:03:21 -0800

    Result Screen Work (40%)

--- Assets/Scripts/UI/ShowResultBar.cs
+++ Assets/Scripts/UI/ShowResultBar.cs
@@ -7,6 +7,10 @@ using System.Linq;
 public class ShowResultBar : MonoBehaviour
 {
     MinigameManager minigameManager;
+    bool scoreSorted;
+    bool test;
+    bool test2 = true;
+    bool test3 = true;
 
     public Image P1_Bar;
     public Image P2_Bar;
@@ -19,7 +23,7 @@ public class ShowResultBar : MonoBehaviour
     int maxScore = 7000;
 
     public List<int> allScoresList = new List<int>();
-    public int[] allScores = new int[5];
+    public int[] allScores = new int[4];
 
     //Tracks and playersScores P1-4 scores in odrder (playersScore1 - highest)
     public int place1, place2, place3, place4;
@@ -32,41 +36,73 @@ public class ShowResultBar : MonoBehaviour
     void Update()
     {
         if (minigameManager.showResultBar)
-            Stuff();
+            SortingScores();
     }
 
-    void Stuff()
+    void SortingScores()
     {
-        //Adds all players' scores to an array
-        allScores[0] = P1_Score; allScores[1] = P2_Score; allScores[2] = P3_Score; allScores[3] = P4_Score;
+        if (!scoreSorted)
+        {
+            //Adds all players' scores to an array
+            allScores[0] = P1_Score; allScores[1] = P2_Score; allScores[2] = P3_Score; allScores[3] = P4_Score;
 
+            //Focking Magic
+            place1 = allScores.Max();               //Gives Place 1 the highest score from all players
+            allScoresList = allScores.ToList();     //Converts Array to List
+            allScoresList.Remove(allScores.Max());  //Deletes the highest score that was given to Place 1
+            allScores = allScoresList.ToArray();    //Converts List back to Array
 
-        //Focking Magic
-        place1 = allScores[0];
-        place1 = allScores.Max();
-        allScoresList = allScores.ToList();
-        allScoresList.Remove(allScores.Max());
-        allScores = allScoresList.ToArray();
+            place2 = allScores.Max();
+            allScoresList = allScores.ToList();
+            allScoresList.Remove(allScores.Max());
+            allScores = allScoresList.ToArray();
 
-        place2 = allScores.Max();
-        allScoresList = allScores.ToList();
-        allScoresList.Remove(allScores.Max());
-        allScores = allScoresList.ToArray();
+            place3 = allScores.Max();
+            allScoresList = allScores.ToList();
+            allScoresList.Remove(allScores.Max());
+            allScores = allScoresList.ToArray();
 
-        place3 = allScores.Max();
-        allScoresList = allScores.ToList();
-        allScoresList.Remove(allScores.Max());
-        allScores = allScoresList.ToArray();
+            place4 = allScores.Max();
+            allScoresList = allScores.ToList();
+            allScoresList.Remove(allScores.Max());
+            allScores = allScoresList.ToArray();    //Now both Array and List are empty but place1, place2, place3, place4 have sorted scores
 
-        place4 = allScores.Max();
-        allScoresList = allScores.ToList();
-        allScoresList.Remove(allScores.Max());
-        allScores = allScoresList.ToArray();
+            scoreSorted = true;
+        }
 
+            StartCoroutine(RaisingResultBar());
+    }
 
-        P1_Bar.fillAmount = Mathf.Lerp(P1_Bar.fillAmount, resultBarAmount(P1_Score, 0, 7000, 0, 1), speed * Time.deltaTime);
-        P2_Bar.fillAmount = Mathf.Lerp(P2_Bar.fillAmount, resultBarAmount(P2_Score, 0, 7000, 0, 1), speed * Time.deltaTime);
-        P3_Bar.fillAmount = Mathf.Lerp(P3_Bar.fillAmount, resultBarAmount(P3_Score, 0, 7000, 0, 1), speed * Time.deltaTime);
+    IEnumerator RaisingResultBar()
+    {
+        if (!test)
+        {
+            P1_Bar.fillAmount = Mathf.Lerp(P1_Bar.fillAmount, resultBarAmount(place3, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            P2_Bar.fillAmount = Mathf.Lerp(P2_Bar.fillAmount, resultBarAmount(place3, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            P3_Bar.fillAmount = Mathf.Lerp(P3_Bar.fillAmount, resultBarAmount(place3, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            yield return new WaitForSeconds(3.5f);
+            test = true;
+            test2 = false;
+            speed = 2f;
+        }
+        if (!test2)
+        {
+            P1_Bar.fillAmount = Mathf.Lerp(P1_Bar.fillAmount, resultBarAmount(place2, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            P2_Bar.fillAmount = Mathf.Lerp(P2_Bar.fillAmount, resultBarAmount(place2, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            P3_Bar.fillAmount = Mathf.Lerp(P3_Bar.fillAmount, resultBarAmount(place2, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            yield return new WaitForSeconds(3.5f);
+            test2 = true;
+            test3 = false;
+            speed = 2f;
+        }
+        if (!test3)
+        {
+            P1_Bar.fillAmount = Mathf.Lerp(P1_Bar.fillAmount, resultBarAmount(place1, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            P2_Bar.fillAmount = Mathf.Lerp(P2_Bar.fillAmount, resultBarAmount(place1, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            P3_Bar.fillAmount = Mathf.Lerp(P3_Bar.fillAmount, resultBarAmount(place1, 0, maxScore, 0, 1), speed * Time.deltaTime);
+            yield return new WaitForSeconds(3.5f);
+            test3 = true;
+        }
     }
 
 
