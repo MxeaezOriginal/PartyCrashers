4b3f6dc22a2ed48077efb2d75e7530cc6d401947
Skadanton <Skadanton@gmail.com>
2017-01-24 17:39:24 -0800

    Reward Screen More Work

--- Assets/Scripts/UI/MinigameManager.cs
+++ Assets/Scripts/UI/MinigameManager.cs
@@ -8,4 +8,6 @@ public class MinigameManager : MonoBehaviour
     public bool scoreSorted;
     public bool barsRaised;
     public bool showRewardCanvas;
+
+    public int P1_place, P2_place, P3_place, P4_place;      //int from 1 - 4
 }
