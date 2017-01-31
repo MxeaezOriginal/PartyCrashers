244d4993dfb3a553514eba7dee1f94241fee600f
lerahk <jamie_shaw@shaw.ca>
2017-01-31 05:22:38 -0800

    Added BGM and Food pickup sound

---
+++ Assets/Scripts/BGMSelection.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGMSelection : MonoBehaviour {
+
+    public AudioSource audio;
+    private AudioClip currentMusic;
+    public AudioClip[] BGMList;
+    public bool swaptest;
+    // Use this for initialization
+    void Start()
+    {
+        
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (swaptest)
+        {
+            swaptest = false;
+            playRandomMusic();
+        }
+        if (!audio.isPlaying)
+        {
+            swaptest = false;
+            playRandomMusic();
+        }
+            
+    }
+    void playRandomMusic()
+    {
+        currentMusic = BGMList[Random.Range(0, BGMList.Length)];
+        audio.clip = currentMusic;
+        audio.Play();
+    }
+}
---
+++ Assets/Scripts/PlayerSFX.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSFX : MonoBehaviour {
+    public AudioSource audioSource;
+    public AudioClip[] ChewSFX;
+    private AudioClip SFXtoPlay;
+    // Use this for initialization
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collectible>()) //chew
+        {
+            SFXtoPlay = ChewSFX[Random.Range(0, ChewSFX.Length)];
+            audioSource.clip = SFXtoPlay;
+            audioSource.Play();
+        }
+
+    }
+}
