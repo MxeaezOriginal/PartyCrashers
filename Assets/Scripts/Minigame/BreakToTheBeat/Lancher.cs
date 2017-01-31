33026162a9f0846c9564c5fd93e3d16c8619083c
Mxeaez <brodyball@gmail.com>
2017-01-31 14:50:59 -0800

    BreakToTheBeat Launcher script fixed (not test yet)

--- Assets/Scripts/Minigame/BreakToTheBeat/Lancher.cs
+++ Assets/Scripts/Minigame/BreakToTheBeat/Lancher.cs
@@ -11,14 +11,20 @@ public class Lancher : MonoBehaviour
     public Transform m_ShotPos;
     private float m_LastShotTime;
     private int m_ramdom;
-    public int m_GoldPer = 20;
-    public int m_BlackPer = 20;
+    //public int m_GoldPer = 20;
+    //public int m_BlackPer = 20;
     private float m_Timer;
+
+    private bool m_Activated;
+    private int m_LauncherRandom;
+    private int m_LauncherPer;
+    public int LanuncherPercentage = 60;
     // Use this for initialization
     void Start()
     {
         m_LastShotTime = Time.time;
         m_Timer = 0f;
+        m_Activated = false;
     }
 
     // Update is called once per frame
@@ -30,18 +36,41 @@ public class Lancher : MonoBehaviour
             m_FireIntervalMin = 3;
             m_FireIntervalMax = 8;
         }
-        if (m_Timer >= 20 && m_Timer < 30)
+        if (m_Timer >= 20/* && m_Timer < 30*/)
         {
             m_FireIntervalMin = 1;
             m_FireIntervalMax = 5;
         }
-        if (m_Timer >= 30)
-        {
-            // game end
-        }
+        //if (m_Timer >= 30)
+        //{
+        //    // game end
+        //}
         bool canShoot = (m_LastShotTime + Random.Range(m_FireIntervalMin, m_FireIntervalMax)) < Time.time;
+
+        // Launcher disable system
+        // method 1: Launcher or not
+        m_LauncherRandom = Random.Range(0, 2);
+        if(m_LauncherRandom == 0)
+        {
+            m_Activated = true;
+        }
+        else
+        {
+            m_Activated = false;
+        }
+        // method 2: percentage to decide launcher or not
+        //m_LauncherPer = Random.Range(0, 101);
+        //if (m_LauncherPer >= 0 && m_LauncherPer <= LanuncherPercentage)
+        //{
+        //    m_Activated = true;
+        //}
+        //else if (m_LauncherPer >= LanuncherPercentage)
+        //{
+        //    m_Activated = false;
+        //}
+
         // Shoot!
-        if (canShoot)
+        if (canShoot && m_Activated)
         {
             if (m_VasePrefeb != null && m_VasePrefeb1 != null && m_VasePrefeb2 != null)
             {
@@ -55,14 +84,14 @@ public class Lancher : MonoBehaviour
                         {
                             shot.GetComponent<VaseSpeed>().velocity = 10;
                         }
-                        if (m_Timer >= 20 && m_Timer < 30)
+                        if (m_Timer >= 20 /*&& m_Timer < 30*/)
                         {
                             shot.GetComponent<VaseSpeed>().velocity = 15;
                         }
-                        if (m_Timer >= 30)
-                        {
-                            // game end
-                        }
+                        //if (m_Timer >= 30)
+                        //{
+                        //    // game end
+                        //}
                     }
                     if (m_ramdom >= 60 && m_ramdom < 80)
                     {
@@ -71,14 +100,14 @@ public class Lancher : MonoBehaviour
                         {
                             shot.GetComponent<VaseSpeed>().velocity = 10;
                         }
-                        if (m_Timer >= 20 && m_Timer < 30)
+                        if (m_Timer >= 20 /*&& m_Timer < 30*/)
                         {
                             shot.GetComponent<VaseSpeed>().velocity = 15;
                         }
-                        if (m_Timer >= 30)
-                        {
-                            // game end
-                        }
+                        //if (m_Timer >= 30)
+                        //{
+                        //    // game end
+                        //}
                     }
                     if (m_ramdom >= 80 && m_ramdom <= 100)
                     {
@@ -87,14 +116,14 @@ public class Lancher : MonoBehaviour
                         {
                             shot.GetComponent<VaseSpeed>().velocity = 10;
                         }
-                        if (m_Timer >= 20 && m_Timer < 30)
+                        if (m_Timer >= 20 /*&& m_Timer < 30*/)
                         {
                             shot.GetComponent<VaseSpeed>().velocity = 15;
                         }
-                        if (m_Timer >= 30)
-                        {
-                            // game end
-                        }
+                        //if (m_Timer >= 30)
+                        //{
+                        //    // game end
+                        //}
                     }
                 }
                 //else
