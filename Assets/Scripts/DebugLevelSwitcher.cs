436e0a99abc5621391e50d49b360c644fc4e797e
Cooperjpk <cooperjpklassen@gmail.com>
2017-02-07 21:18:32 -0800

    More fixes

--- Assets/Scripts/DebugLevelSwitcher.cs
+++ Assets/Scripts/DebugLevelSwitcher.cs
@@ -2,41 +2,39 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class DebugLevelSwitcher : MonoBehaviour {
+public class DebugLevelSwitcher : MonoBehaviour
+{
 
-	void Update ()
+    void Update()
     {
-        if (Debug.isDebugBuild)
+        if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                SceneManager.LoadScene("MainMenu");
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                SceneManager.LoadScene("Lobby_01");
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                SceneManager.LoadScene("Lobby_02");
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha4))
-            {
-                SceneManager.LoadScene("Lobby_03");
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha5))
-            {
-                SceneManager.LoadScene("DiningRoom");
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha6))
-            {
-                SceneManager.LoadScene("BowlingRoom");
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha7))
-            {
-                SceneManager.LoadScene("KaminsRoom");
-            }
+            SceneManager.LoadScene("MainMenu");
         }
-
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("Lobby_01");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SceneManager.LoadScene("Lobby_02");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            SceneManager.LoadScene("Lobby_03");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            SceneManager.LoadScene("DiningRoom");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            SceneManager.LoadScene("BowlingRoom");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            SceneManager.LoadScene("KaminsRoom");
+        }
     }
+
 }
