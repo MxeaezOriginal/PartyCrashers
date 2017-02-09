626fdefad3ae63cc0570037a648a35b60a15bf42
Mxeaez <brodyball@shaw.ca>
2017-02-09 15:39:31 -0800

    Testing inputs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -146,7 +146,7 @@ public class Player : MonoBehaviour
         }
 
         //Primary Attack
-        if (Input.GetAxisRaw(m_PrimaryAttack + m_Controller.ToString()) == 1)
+        if (Input.GetButtonDown(m_PrimaryAttack + m_Controller.ToString()))
         {
             if (!m_CantAttack)
             {
@@ -155,7 +155,7 @@ public class Player : MonoBehaviour
         }
 
         //Secondary Attack
-        if (Input.GetAxisRaw(m_SecondaryAttack + m_Controller.ToString()) == 1)
+        if (Input.GetButtonDown(m_SecondaryAttack + m_Controller.ToString()))
         {
             if (!m_CantAttack && GetComponent<PlayerController>().m_CantMove == false)
             {
