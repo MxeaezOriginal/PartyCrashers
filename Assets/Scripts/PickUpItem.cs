b0467df5e0c2e6b63dc54316729f33a4120432df
Kyoung2725 <lightningx2725@gmail.com>
2017-02-14 12:05:46 -0800

    food is stable yet bugged

--- Assets/Scripts/PickUpItem.cs
+++ Assets/Scripts/PickUpItem.cs
@@ -2,11 +2,23 @@
 using System.Collections;
 
 public class PickUpItem : MonoBehaviour
-{
+{ 
+	//VfX
+	public GameObject getpickupVFX;
+	//VFX end
+	 
     void OnTriggerStay(Collider other)
     {
         if (other.tag == ("Player"))
         {
+			//VfX
+			if (getpickupVFX != null) 
+			{
+				GameObject getfood;
+				getfood = (GameObject)Instantiate (getpickupVFX, transform.position, transform.rotation);
+				Destroy (getfood, 0.5f);
+			}
+			//VFX end
             gameObject.SetActive(false);
         }
     }
