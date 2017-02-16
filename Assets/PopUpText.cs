a798b3f2acf77f33e27ebcf9153d98871142a2e4
daanilo1 <dmdanilo8@gmail.com>
2017-02-15 23:51:57 -0800

    UI stuff

---
+++ Assets/PopUpText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUpText : MonoBehaviour {
+    [Header("Canvas")]
+    public GameObject[] canvases;
+
+    void Update () {
+
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        canvases[0].SetActive(true);
+        Debug.Log("PopTxtActivated");
+    }
+
+    void OnTriggerExit()
+    {
+        canvases[0].SetActive(false);
+        Debug.Log("PopTxtDesactivated");
+    }
+}
