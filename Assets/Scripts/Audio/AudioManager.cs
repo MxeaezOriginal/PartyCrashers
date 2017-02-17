504895a2242bd5a393bc568aba06791c01941ebe
NGAEVA <ywt1230@163.com>
2017-02-16 17:15:28 -0800

    AudioManager created

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource m_AudioManager;
+    private AudioClip m_CurMusic;
+    public AudioClip[] m_RandomMusicList;
+    public float m_DelayTime;
+    public float m_timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void playMusic()
+    {
+        m_CurMusic = m_RandomMusicList[Random.Range(0, m_RandomMusicList.Length)];
+        m_AudioManager.clip = m_CurMusic;
+        m_AudioManager.Play();
+    }
+}
