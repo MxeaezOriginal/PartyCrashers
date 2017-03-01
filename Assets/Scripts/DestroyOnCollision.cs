50824c387203d5d4b3d4514d584a973612b994ca
lerahk <james1996shaw@gmail.com>
2017-02-28 20:50:11 -0800

    Shooter Bullet SFX

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -7,6 +7,9 @@ public class DestroyOnCollision : MonoBehaviour
     private float LifeTimer;
     private Collider col;
 
+    //James VFX code
+    public GameObject hitVFX;
+    //James VFX code
     // Use this for initialization
     void Start()
     {
@@ -16,6 +19,14 @@ public class DestroyOnCollision : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        //hames VFX code
+        if (hitVFX != null)
+        {
+            GameObject hitParticle;
+            hitParticle = (GameObject)Instantiate(hitVFX, transform.position, transform.rotation);
+            Destroy(hitParticle, 1f);
+        }
+        //James VFX code
         if (other.gameObject.GetComponent<WeaponID>() == null) 
         {
             DestroyBullet();
