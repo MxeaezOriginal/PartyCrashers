76caef846e02b2f12ee3e41f795979d550838d30
Mxeaez <361466@AIV.EDMC.STU>
2017-03-02 16:44:17 -0800

    Shooter enemy does damage

---
+++ Assets/Scripts/AI/EnemyDamage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public int m_Damage;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<HeartSystem>() != null)
+        {
+            HeartSystem playerHealth = other.gameObject.GetComponent<HeartSystem>();
+            playerHealth.TakeDamage(m_Damage);
+            playerHealth.UpdateHearts();
+        }
+    }
+}
