f3581782f64fe5a26e163df3354d8bc0c7ff9aa5
Skadanton <375449@AIV.EDMC.STU>
2017-03-09 15:39:22 -0800

    I don't even know what I've done at this point

---
+++ Assets/Scripts/UI/EndCanvas.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class EndCanvas : MonoBehaviour
+{
+    public bool activated;
+    Canvas endCanvas;
+    Button backToMainMenuButton;
+
+    EventSystem ES;
+    StandaloneInputModule SIM;
+
+    void Awake()
+    {
+        endCanvas = GameObject.Find("End Canvas").GetComponent<Canvas>();
+        backToMainMenuButton = transform.GetChild(1).GetComponent<Button>();
+
+        ES = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        SIM = GameObject.Find("EventSystem").GetComponent<StandaloneInputModule>();
+    }
+
+    void Update()
+    {
+        if(!activated)
+        {
+            endCanvas.enabled = false;
+            foreach (GameObject player in GameManager.m_Instance.m_Players)
+            {
+                player.GetComponent<PlayerController>().m_CantMove = false;
+            }
+        }
+        else
+        {
+            endCanvas.enabled = true;
+            foreach (GameObject player in GameManager.m_Instance.m_Players)
+            {
+                player.GetComponent<PlayerController>().m_CantMove = true;
+            }
+
+
+            SIM.horizontalAxis = ("Horizontal_" + GameManager.m_Instance.m_Player1.m_Controller);
+            SIM.verticalAxis = ("Vertical_" + GameManager.m_Instance.m_Player1.m_Controller);
+            SIM.submitButton = ("Submit_" + GameManager.m_Instance.m_Player1.m_Controller);
+
+            ES.enabled = false;
+            ES.enabled = true;
+            ES.SetSelectedGameObject(backToMainMenuButton.gameObject);
+        }
+    }
+
+    public void BackToMainMenu()
+    {
+        print("EXIT TO MAIN MENU");
+    }
+}
