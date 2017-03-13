67ce39e77fb352d13efcd1ac088f7c684fc2d74b
NGAEVA <ywt1230@163.com>
2017-03-12 21:55:01 -0700

    Door sfx added

--- Assets/Scripts/BreakableDoor.cs
+++ Assets/Scripts/BreakableDoor.cs
@@ -7,6 +7,13 @@ public class BreakableDoor : MonoBehaviour {
     public GameObject explosionObject;
     public GameObject particleLocation;
 
+    public AudioSource audioSource;
+    public AudioClip[] DoorSFX;
+    private AudioClip SFXtoPlay;
+    public float maxRandomPitch;
+    public float minRandomPitch;
+    private float randomPitch;
+
     public void OnTriggerStay(Collider other)
     {
         if (other.tag == "Projectile")
@@ -24,6 +31,12 @@ public class BreakableDoor : MonoBehaviour {
             explosion.transform.localScale = new Vector3(explosionRadius, explosionRadius, explosionRadius);
             Destroy(gameObject);
         }
+
+        randomPitch = Random.RandomRange(maxRandomPitch, minRandomPitch);
+        SFXtoPlay = DoorSFX[Random.Range(0, DoorSFX.Length)];
+        audioSource.clip = SFXtoPlay;
+        audioSource.pitch = randomPitch;
+        audioSource.Play();
     }
 
 }
--- Assets/Scripts/Trap/TrapAnimation.cs
+++ Assets/Scripts/Trap/TrapAnimation.cs
@@ -5,8 +5,15 @@ public class TrapAnimation : MonoBehaviour {
 
     public Animator ani;
     public GameObject m_effect;
-	// Use this for initialization
-	void Start () {
+
+    public AudioSource audioSource;
+    public AudioClip[] DoorSFX;
+    private AudioClip SFXtoPlay;
+    public float maxRandomPitch;
+    public float minRandomPitch;
+    private float randomPitch;
+    // Use this for initialization
+    void Start () {
         ani.enabled = false;
 	}
 	
@@ -17,7 +24,7 @@ public class TrapAnimation : MonoBehaviour {
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player" && ani.enabled == false)
+        if(other.tag == "Player" && ani.enabled == false && other.GetComponent<Player>().m_State == Player.State.Alive)
         {
             ani.enabled = true;
             if (m_effect != null)
@@ -26,6 +33,12 @@ public class TrapAnimation : MonoBehaviour {
                 effect = (GameObject)Instantiate(m_effect, gameObject.transform.position, gameObject.transform.rotation);
                 Destroy(effect, 3f);
             }
+
+            randomPitch = Random.RandomRange(maxRandomPitch, minRandomPitch);
+            SFXtoPlay = DoorSFX[Random.Range(0, DoorSFX.Length)];
+            audioSource.clip = SFXtoPlay;
+            audioSource.pitch = randomPitch;
+            audioSource.Play();
         }
 
 
