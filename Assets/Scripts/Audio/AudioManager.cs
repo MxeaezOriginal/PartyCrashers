fcdd44ae16ff5f5b806feeb147b4bc94e9c00911
Mxeaez <brodyball@shaw.ca>
2017-03-15 02:55:58 -0700

    Changed audiomanager a bit

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -55,7 +55,7 @@ public class AudioManager : MonoBehaviour {
             //m_RandomMusicList.Add(ac);
             //m_AudioSource.clip = ac;
             //m_AudioSource.pitch = m_pitch;
-            AudioSource.PlayClipAtPoint(ac, Camera.main.transform.position);
+            m_AudioSource.PlayOneShot(ac, 1);
             m_IsPlaying = true;
             m_DelayCounter = m_DelayTime;
         }
